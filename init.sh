9c89c5f43c7d477eafa2472d4516a714824b4bcd
Shigeo Kitamura <shigeo9462@gmail.com>
2020-02-22 19:54:57 +0900

    fix terminal

--- init.sh
+++ init.sh
@@ -4,10 +4,12 @@
 zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+ln -fs $HOME/dotfiles/terminal/zlogin    $HOME/.zlogin
+ln -fs $HOME/dotfiles/terminal/zlogout   $HOME/.zlogout
+ln -fs $HOME/dotfiles/terminal/zpreztorc $HOME/.zpreztorc
+ln -fs $HOME/dotfiles/termianl/zprofile  $HOME/.zprofile
+ln -fs $HOME/dotfiles/terminal/zshenv    $HOME/.zshenv
+ln -fs $HOME/dotfiles/terminal/zshrc     $HOME/.zshrc
 
 chsh -s /bin/zsh
 
